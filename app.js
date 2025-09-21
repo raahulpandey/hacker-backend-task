fb7da97d2d94e082ebe681651ef6b52308290485
raahulpandey <arryaroy7367@gmail.com>
2025-09-21 14:49:52 +0530

    level 1 task

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+
+app.use((req,res,next) => {
+    const now = new Date().toISOString();
+    console.log(`[${now} ${req.method} ${req.url}]`)
+    next();
+})
+
+app.get('/ping',(req,res) => {
+    res.status(200).json({message :'pong'})
+})
+
+app.get('/time',(req,res) => {
+    res.status(200).json({time :new Date().toISOString()})
+})
+
+app.get('/random',(req,res) => {
+    const val = Math.floor(Math.random*100) + 1
+    res.status(200).json({random : val})
+})
+
+app.use((req,res) => {
+    res.status(404).json({error :'value not found'})
+})
+
+app.listen(5000,() => {
+    console.log('server is running on port 5000')
+})
